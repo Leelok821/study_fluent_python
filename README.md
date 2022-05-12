2ecb95c4bdca41e4852e08f986ceef71593e5ccf
lee <504455367@qq.com>
2022-05-12 18:35:13 +0800

    update .ignore

---
+++ README.md
@@ -0,0 +1,1 @@
+# study_fluent_python
---
+++ python数据模型/1_1_一摞有序的纸牌.py
@@ -0,0 +1,38 @@
+# -*- coding:utf-8 -*-
+
+"""
+# Author ：li zi hao
+"""
+
+import collections
+from random import choice
+
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+
+class FrenchDeck:
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+    suits = 'spades hearts diamonds clubs '.split()
+
+    def __init__(self):
+        self.cards = [Card(rank, suit)
+                      for suit in self.suits for rank in self.ranks]
+
+    def __len__(self):
+        return len(self.cards)
+
+    def __getitem__(self, item):
+        return self.cards[item]
+
+
+if __name__ == '__main__':
+    deck = FrenchDeck()
+    suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+
+    def spades_high(card):
+        suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+        rank_value = FrenchDeck.ranks.index(card.rank)
+        return rank_value * len(suit_values) + suit_values[card.suit]
+
+    for card in sorted(deck, key=spades_high):
+        print(card)
